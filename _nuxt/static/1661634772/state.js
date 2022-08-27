45847039c0ba413820cc050c8b444ba3429e866f
hewerthomn <hewerthomn@users.noreply.github.com>
2022-08-27 21:12:52 +0000

    deploy: d338f615b9a4d271ffb9148386b551785388870b

---
+++ _nuxt/static/1661634772/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661634772",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ca8a4a75"}}}}(null,"\u002F"));
