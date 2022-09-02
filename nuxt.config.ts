bd17d036fcb9b3a07b0d6f03bc1b94afaffdb399
Éverton <hewerthomn@gmail.com>
2022-09-01 20:15:29 -0400

    Either Hot Shit or Total Bollocks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
+import { defineNuxtConfig } from '@nuxt/bridge'
 
-export default {
-  /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
+export default defineNuxtConfig({
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   router: {
@@ -41,16 +42,19 @@ export default {
     '~/css/bootstrap.min.css',
     '~/css/styles.css'
   ],
+
   /*
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [],
+
   /*
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
   */
   components: true,
+
   /*
   ** Nuxt.js dev-modules
   */
@@ -58,22 +62,21 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module'
   ],
+
   /*
   ** Nuxt.js modules
   */
   modules: [
-    // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
+    // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt'
   ],
+
   /*
-  ** Content module configuration
-  ** See https://content.nuxtjs.org/configuration
-  */
-  content: {},
-  /*
-  ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
   */
-  build: {}
-}
+  build: {
+    babel: {
+      compact: true
+    }
+  }
+})
